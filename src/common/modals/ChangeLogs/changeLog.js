3e137e34b70deb11073f1fd16ccb3773fe6a0b8c
MG8853 <qq3082499405@icloud.com>
2022-05-17 13:58:33 +0900

    v1.1.23-felnull.1 - GD update

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,32 +1,10 @@
 module.exports = {
-  new: [
-    {
-      header: 'インスタンスマネージャーにPlayボタンを追加しました。',
-      content: '',
-      advanced: { cm: '' }
-    },
-    {
-      header: 'modsにカテゴリ選択を追加しました。',
-      content: '',
-      advanced: { cm: '' }
-    }
-  ],
-  improvements: [
-    {
-      header: 'CurseForge APIが新しいAPIに移行されました。',
-      content: 'まだうまく動作しない可能性があります。',
-      advanced: { cm: '' }
-    },
-    {
-      header: 'RAMメモリスライダーに選択したMBを表示するようにしました。',
-      content: '使いやすくなるはず...',
-      advanced: { cm: '' }
-    }
-  ],
+  new: [{}],
+  improvements: [{}],
   bugfixes: [
     {
-      header: '古いインスタンスが開始しない問題を修正しました。',
-      content: '',
+      header: 'CurseForgeAPIを変更しました',
+      content: 'これでGDも復活じゃーーー！',
       advanced: { cm: '' }
     }
   ]
